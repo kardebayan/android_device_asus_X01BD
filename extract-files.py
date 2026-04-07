dbb2d06a3d3588c58541c5e44080a359dbc7b595
Debayan Kar <kardebayan3@gmail.com>
2026-04-07 17:05:24 +0000

    X01BD: Drop QTI fingerprint interfaces
    Change-Id: I5858343d170ba69bc53f4fc2e967a550e2892ec7

--- extract-files.py
+++ extract-files.py
@@ -28,8 +28,6 @@ def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
 
 
 blob_fixups: blob_fixups_user_type = {
-    'product/etc/permissions/qti_fingerprint_interface.xml': blob_fixup()
-        .regex_replace('/system/framework/', '/system/product/framework/'),
     'vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service_asus.rc': blob_fixup()
         .regex_replace('android.hardware.biometrics.fingerprint@2.1-service', 'android.hardware.biometrics.fingerprint@2.1-service_asus'),
     ('vendor/lib/libmmcamera_faceproc.so', 'vendor/lib/libmmcamera_faceproc2.so'): blob_fixup()
